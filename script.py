9381f1e389bdf841a6ae2d23ad993642bb53fa31
Lang Tran <lang.tran96@gmail.com>
2018-04-16 00:42:20 +0000

    updated script

--- script.py
+++ script.py
@@ -4,19 +4,13 @@ DIR_NAME = "./data/"
 PROTOCOL_LIST = ['DHCP', 'DNS', 'HTTP', 'HTTP/XML', 'ICMP', 'ICMPv6', 'IGMPv3', 'LLMNR', 'MDNS', 'NBNS', 'OCSP', 'QUIC', 'SSDP', 'SSL', 'SSLv2', 'TCP', 'TLSv1', 'TLSv1.2', 'TLSv1.3', 'UDP', 'ARP']
 df = pd.read_csv(DIR_NAME + 'pcap.csv')
 
-"""
-for i in protocol_list:
-     temp=df[df['Protocol']==i]
-     if i=='HTTP/XML':
-         temp[['Destination','Length']].groupby(['Destination']).count().to_csv('HTTP_XML.csv')
-     else:
-         temp[['Destination','Length']].groupby(['Destination']).count().to_csv(i+'.csv')
-"""
-
-def example():
+def group_by(col):
+    grouped = df.groupby([col])[col].count().sort(['count'])
+    return grouped
 
 def main():
-    print "test"
+    print df.describe()
+    print group_by('Source')
 
 if __name__ == '__main__':
     main()
