5c851b34b8d45116d3371458cac9af983b313e23
ChengShen1996 <617984532@qq.com>
2018-04-15 17:31:50 -0700

    1,2,3

--- script.py
+++ script.py
@@ -14,10 +14,23 @@ for i in protocol_list:
 """
 
 def example():
-
+	pass
+def Dest_rece_most():
+	temp=df1.groupby(['Source']).sum().sort_values('Length',ascending=False)
+	temp.to_csv('data/Src_send_most.csv')
+def Src_send_most():
+	temp=df1.groupby(['Destination']).sum().sort_values('Length',ascending=False)
+	temp.to_csv('data/Dest_rec_most.csv')
+def Count_packet_type():
+	for i in PROTOCOL_LIST:
+		temp =df[df['Protocol']==i]
+		if i=='HTTP/XML':
+			temp.groupby(['Protocol']).count().to_csv('HTTP_XML.csv')
+		else:
+			temp.groupby(['Protocol']).count().to_csv(i+'.csv')
 def main():
-    print "test"
+    Count_packet_type()
+
 
 if __name__ == '__main__':
     main()
-    pass
--- script1.py
+++
@@ -1,7 +0,0 @@
-import pandas as db
-df1 = db.read_csv('data/pcap.csv')
-#temp=df1.groupby(['Source']).sum().sort_values('Length',ascending=False)
-#temp.to_csv('data/Src_send_most.csv')
-
-temp=df1.groupby(['Destination']).sum().sort_values('Length',ascending=False)
-temp.to_csv('data/Dest_rec_most.csv')
